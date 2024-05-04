866493f3256472a85f41144449cf4d8374d8334b
bhagat-abhishek <inst.abhishek@gmail.com>
2024-05-05 02:24:14 +0530

    Saving file, updating the saved file

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const { app, BrowserWindow, ipcMain, dialog } = require("electron");
 const path = require("node:path");
 const fs = require("fs");
 
+let savedFilePath = undefined;
+
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
@@ -14,15 +16,23 @@ const createWindow = () => {
   // Listening to IPC
   ipcMain.on("save", async (event, text) => {
     // Save the text to a file
-    let fullpath = await dialog.showSaveDialog(win, {
-      defaultPath: "filename.txt",
-    });
 
-    if (!fullpath.canceled) {
-      fs.writeFile(fullpath.filePath, text, (err) => {
-        if (err) console.log("There was an error saving the file.", err);
-        console.log("File has been saved.");
+    // Checking if we have a saved file path
+    if (savedFilePath === undefined) {
+      let fullpath = await dialog.showSaveDialog(win, {
+        defaultPath: "filename.txt",
       });
+
+      // checking if the user cancled te dialog
+      if (!fullpath.canceled) {
+        // Updating the file path before saving
+        savedFilePath = fullpath.filePath;
+        // saving - writing data to a file
+        writeToFile(savedFilePath, text);
+      }
+    } else {
+      // saving - writing data to a file
+      writeToFile(savedFilePath, text);
     }
   });
 
@@ -44,3 +54,12 @@ app.whenReady().then(() => {
     if (process.platform !== "darwin") app.quit();
   });
 });
+
+
+// Function to write file to a given path with the data
+function writeToFile(path, data) {
+  fs.writeFile(path, data, (err) => {
+    if (err) console.log("There was an error saving the file.", err);
+    console.log("File has been saved.");
+  });
+}
